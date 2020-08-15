e60c2b1905609a6c47f1e3e642b5a73da79af0c6
jasso208 <jasso.gallegos@gmail.com>
2020-08-15 02:05:28 -0500

    mejoras

--- job_diario.py
+++ job_diario.py
@@ -1,7 +1,7 @@
 import requests
-# Creamos la petición HTTP con GET:
+# Creamos la peticion HTTP con GET:
 r = requests.get("https://empeno.jassdel.com/empenos/api_job_diario/")
-# Imprimimos el resultado si el código de estado HTTP es 200 (OK):
+# Imprimimos el resultado si el codigo de estado HTTP es 200 (OK):
 if r.status_code == 200:
 	print("Exito")
 else:
