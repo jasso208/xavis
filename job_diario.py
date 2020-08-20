0cdda2e3fb8ad9635a4705234a6780b8d530d262
jasso208 <jasso.gallegos@gmail.com>
2020-08-19 22:46:53 -0500

    Na

--- job_diario.py
+++ job_diario.py
@@ -1,12 +1,10 @@
 import requests
 
-print(date.today())
-
 print("Iniciamos proceso boletas vencidas")
 #ejecutamos el proceso para detectar boletas o periodos vencidos.
 #https://empeno.jassdel.com/
-r = requests.get("http://127.0.0.1:8000/empenos/api_job_diario/")
-# Imprimimos el resultado si el código de estado HTTP es 200 (OK):
+r = requests.get("https://empeno.jassdel.com/empenos/api_job_diario/")
+# Imprimimos el resultado si el codigo de estado HTTP es 200 (OK):
 
 if r.status_code == 200:
 	print("Exito")
@@ -16,7 +14,7 @@ else:
 
 print("Iniciamos proceso envio reporte cajas abiertas")
 #https://empeno.jassdel.com/
-r = requests.get("http://127.0.0.1:8000/empenos/api_notificacion_cajas_abiertas/")
+r = requests.get("https://empeno.jassdel.com/empenos/api_notificacion_cajas_abiertas/")
 
 if r.status_code == 200:
 	print("Exito")
