193ff74619ace959106d5c9bb9f186f91792e2c6
jasso208 <jasso.gallegos@gmail.com>
2020-12-02 14:12:32 -0600

    Se agrega mejoras a la apertura de caja para evitar el doble click

--- empenos/models.py
+++ empenos/models.py
@@ -891,6 +891,7 @@ class Pagos_Temp(models.Model):
 #esta tabla debera llenarse cada inicio de año para marcar los dias no habiles del año y que la fecha de vencimiento no caiga en estos dias.
 class Dia_No_Laboral(models.Model):
     fecha=models.DateTimeField()
+    
     class Meta:
         unique_together=("fecha",)
 
