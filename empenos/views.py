f28b58b54c0879b45ef3b551cabd2de26eedd7a8
jasso208 <jasso.gallegos@gmail.com>
2021-02-19 12:30:56 -0600

    mejoras en apartado

--- empenos/views.py
+++ empenos/views.py
@@ -556,35 +556,34 @@ def concepto_retiro(request):
 #*¨**************************************************************************************************************************************************************
 @transaction.atomic
 def apartado(request):
-	#si no esta logueado mandamos al login
-	if not request.user.is_authenticated:
-		return HttpResponseRedirect(reverse('seguridad:login'))
-	
-	#si el usuario y contraseña son correctas pero el perfil no es el correcto, bloquea el acceso.
-	try:
-		user_2=User_2.objects.get(user=request.user)
-	except Exception as e:		
-		print(e)
-		form=Login_Form(request.POST)
-		estatus=0
-		msj="La cuenta del usuario esta incompleta."			
-		return render(request,'login.html',locals())
 
 	pub_date = date.today()
 	min_pub_date_time = datetime.combine(pub_date, time.min) 
 	max_pub_date_time = datetime.combine(pub_date, time.max)  
 
-	try:		
-		#validamos si el usuario tiene caja abierta en el dia actual.
-		caja=Cajas.objects.get(fecha__range=(min_pub_date_time,max_pub_date_time),fecha_cierre__isnull=True,usuario=request.user)
+
+
+	#si regresa none, es porque el usuario no esta logueado.
+	user_2_1 = User_2.fn_is_logueado(request.user)
+	if user_2_1 == None:
+		return HttpResponseRedirect(reverse('seguridad:login'))
+
+	#obtenemos el usuario virtual de la sucursal.
+	#ya que sobre este usuario se registran los ingresos.
+	user_2 = User_2.objects.get(user = user_2_1.sucursal.usuario_virtual)
+
+	#validamos si el usuario tiene caja abierta
+	caja = user_2.fn_tiene_caja_abierta()
+
+	if caja == None:
+		caja_abierta="0"
+		caja=Cajas
+	else:
 		caja_abierta="1"#si tiene caja abierta enviamos este estatus para  dejar entrar a la pantalla.
 		suc=caja.sucursal
 		c=caja.caja
 
-	except Exception as e:
-		print(e)
-		caja_abierta="0"
-		caja=Cajas
+
 
 	username=request.user.username
 	id_sucursal=user_2.sucursal.id
